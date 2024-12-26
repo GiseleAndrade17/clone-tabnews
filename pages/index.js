631c7d191f7530a3c329293b9670a08e04501fc9
GiseleAndrade17 <giselemariadeandrade@gmail.com>
2024-12-26 20:17:30 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home (){
-    return <h1> Estou aprendendo programação.
-        Tenho que treinar muito. </h1>;
+function Home() {
+  return <h1> Estou aprendendo programação. Tenho que treinar muito. </h1>;
 }
 
 export default Home;
